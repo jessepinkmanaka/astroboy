614fc1b51b340505de03815bc1014ff6b33fb9fc
jessepinkmanaka <jessepinkmanaka@gmail.com>
2025-07-19 00:49:59 +0330

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -124,7 +124,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://astroboy-production.up.railway.app',
+]
 
 
 # Default primary key field type
